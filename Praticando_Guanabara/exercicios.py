a431bc65f685dc647c74e3002d60b64e4569a1e6
Vanessa Do Santos <vanessa.pitu@gmail.com>
2025-01-24 09:17:34 -0300

    Aulas
    praticando o que aprendi

--- Praticando_Guanabara/exercicios.py
+++ Praticando_Guanabara/exercicios.py
@@ -1,10 +1,16 @@
 print ("Olá, meu xuxuzinho!")
 #Saudação
 nome = input("Qual é o seu nome?")
-print ("Então "nome" Seja muito bem vindo!")
+print ("Então", nome ,"Seja muito bem vindo!")
 #Calcular a idade
-print ("Então vamos calcular a sua idade")
-idade= input ("Qual a sua idade?")
-dias= int(idade)*365
-print("Você tem, "idade",anos, o que equivale a aproximadamente,"dias"")
+ano_nascimeto = int (input ("Digite o ano em que você nasceu:"))
+ano_atual = int (input("Digite o ano atual:"))
+idade = ano_atual - ano_nascimeto
+print (f"Você tem ou fará {idade} anos de idade.")
+#Formatação data de nascimento
+dia = input ("Qual é o dia do seu nascimento?")
+mes = input ("Qual o mês do seu nascimento?")
+ano = input ("Qual é o ano do seu nascimento?")
+data_nascimento = dia+ "/" +mes+ "/" +ano
+print (f"Você nasceu no dia {dia}/{mes}/{ano}")
 
