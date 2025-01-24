0c3076986f13b27cf406ea5ee9f917d6b4c5c98e
Vanessa Do Santos <vanessa.pitu@gmail.com>
2025-01-24 07:20:31 -0300

    Create exercicios.py

---
+++ Praticando_Guanabara/exercicios.py
@@ -0,0 +1,10 @@
+print ("Olá, meu xuxuzinho!")
+#Saudação
+nome = input("Qual é o seu nome?")
+print ("Então "nome" Seja muito bem vindo!")
+#Calcular a idade
+print ("Então vamos calcular a sua idade")
+idade= input ("Qual a sua idade?")
+dias= int(idade)*365
+print("Você tem, "idade",anos, o que equivale a aproximadamente,"dias"")
+
