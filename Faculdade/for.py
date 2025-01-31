4a08c87697dd167efcb447987d29cdadf059b4a9
Vanessa Do Santos <vanessa.pitu@gmail.com>
2025-01-31 16:43:36 -0300

    att
    Atualização dos aprendizados

--- Faculdade/for.py
+++ Faculdade/for.py
@@ -1,3 +1,19 @@
-list (range (6))
+list(range(6))
+list (range (12, -1, -2))
+
+for tabuada in range (1,11):
+    for numero in range (1,11):
+        valor = tabuada * numero
+        print (f"{tabuada} * {numero} = {valor}")
+
+for x in range (1,11):
+    if x == 3:
+        break 
+    print (x)
+
+for x in range (1,11):
+    if x ==3 :
+        continue
+    print (x)
 
 
